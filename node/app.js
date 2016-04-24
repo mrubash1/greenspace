5e2ca7a178df04567f84cd7e399d14091459780b
Mithun Manivannan <mithunm93@gmail.com>
2016-04-24 03:13:04 -0700

    Allow visit calculation
    Allow estimation of visit probability based on other
    factors. Change the map iframe to new map.

--- node/app.js
+++ node/app.js
@@ -24,6 +24,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', routes);
 app.use('/users', users);
+app.use('/static', express.static('csv'));
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
